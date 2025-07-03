7661e5bb57ab29e83c2df87e15db22c2c202862c
Igor Ferrão <igor.cuber97@gmail.com>
2025-07-03 15:24:09 -0300

    last adjusts

--- languages/lang.js
+++ languages/lang.js
@@ -2,7 +2,7 @@ const translations = {
   pt: {
     hi: "Oi,",
     name: "Sou Igor Ferrão",
-    role: "UX UI Designer na Xgrow",
+    role: "UX UI Designer na Xgrow Learning Platform",
     paragraph1: "Designer de Produto (UX/UI) com mais de 5 anos de experiência na criação de produtos digitais escaláveis, funcionais e centrados no usuário.",
     paragraph2: "Atuo do conceito à entrega de soluções digitais completas, com foco em criação de Design Systems, componentes avançados no Figma e interfaces eficientes que garantem consistência entre times de design e desenvolvimento.",
     paragraph3: "Sou graduado em Sistemas de Informação, o que me permite integrar design e tecnologia, colaborando com equipes multidisciplinares em ambientes ágeis e remotos.",
@@ -13,7 +13,7 @@ const translations = {
   en: {
     hi: "Hi,",
     name: "I'm Igor Ferrão",
-    role: "UX UI Designer at Xgrow",
+    role: "UX UI Designer at Xgrow Learning Platform",
     paragraph1: "Product Designer (UX/UI) with over 5 years of experience in creating scalable, functional, and user-centered digital products.",
     paragraph2: "I work from concept to delivery of complete digital solutions, focusing on the creation of Design Systems, advanced components in Figma, and efficient interfaces that ensure consistency across design and development teams.",
     paragraph3: "I hold a degree in Information Systems, which allows me to bridge design and technology effectively, collaborating with multidisciplinary teams in agile and remote environments.",
@@ -24,7 +24,7 @@ const translations = {
   es: {
     hi: "Hola,",
     name: "Soy Igor Ferrão",
-    role: "UX UI Designer en Xgrow",
+    role: "UX UI Designer en Xgrow Learning Platform",
     paragraph1: "Diseñador de Productos (UX/UI) con más de 5 años de experiencia en la creación de productos digitales escalables, funcionales y centrados en el usuario.",
     paragraph2: "Trabajo desde el concepto hasta la entrega de soluciones digitales completas, con enfoque en la creación de Design Systems, componentes avanzados en Figma e interfaces eficientes que aseguran consistencia entre equipos de diseño y desarrollo.",
     paragraph3: "Soy graduado en Sistemas de Información, lo que me permite unir diseño y tecnología de forma efectiva, colaborando con equipos multidisciplinarios en entornos ágiles y remotos.",
