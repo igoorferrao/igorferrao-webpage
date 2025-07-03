cd7da60857dc70feb2db206da13f3bb1128d8b3a
Igor Ferrao <igor.cuber97@gmail.com>
2025-07-03 19:36:48 -0300

    adjustments made at languages and ending sections

--- languages/lang.js
+++ languages/lang.js
@@ -2,14 +2,61 @@ const translations = {
   pt: {
     hi: "Oi,",
     name: "Sou Igor Ferrão",
-    role: "UX UI Designer na Xgrow Learning Platform",
+    role: "Designer UX UI na Xgrow Learning Platform",
     paragraph1: "Designer de Produto (UX/UI) com mais de 5 anos de experiência na criação de produtos digitais escaláveis, funcionais e centrados no usuário.",
-    paragraph2: "Atuo do conceito à entrega de soluções digitais completas, com foco em criação de Design Systems, componentes avançados no Figma e interfaces eficientes que garantem consistência entre times de design e desenvolvimento.",
-    paragraph3: "Sou graduado em Sistemas de Informação, o que me permite integrar design e tecnologia, colaborando com equipes multidisciplinares em ambientes ágeis e remotos.",
-    paragraph4: "Aplico métodos como: Arquitetura da Informação | Protótipos de Baixa e Alta Fidelidade | Testes de Usabilidade | Pesquisas Qualitativas e Quantitativas | Design Centrado no Usuário | Design Thinking | Ágil | Lean UX",
-    paragraph5: "Acredito em um design que equilibra estética, acessibilidade, estratégia e resultados. Guiado por valores sólidos, busco sempre criar experiências que realmente gerem impacto positivo.",
-    contact: "Contato"
+    paragraph2: "Trabalho desde o conceito até a entrega de soluções digitais completas, com foco na criação de Design Systems, componentes avançados no Figma e interfaces eficientes que garantem consistência entre design e desenvolvimento.",
+    paragraph3: "Sou graduado em Sistemas de Informação, o que me permite conectar design e tecnologia de forma eficaz, colaborando com equipes multidisciplinares em ambientes ágeis e remotos.",
+    paragraph4: "Aplico métodos como:<br>Arquitetura da Informação | Protótipos de Baixa e Alta Fidelidade | Testes de Usabilidade | Pesquisas Qualitativas e Quantitativas | Design Centrado no Usuário | Design Thinking | Ágil | Lean UX",
+    paragraph5: "Acredito em um design que equilibra estética, acessibilidade, estratégia e resultados. Guiado por valores fortes, estou sempre buscando criar experiências que realmente causem um impacto positivo.",
+    contact: "Contato",
+
+    // Nova seção
+    originTitle: "Igor Ferrão, nascido e baseado no Brasil.",
+    originText: "Trabalhando com empresas que têm UX como parte de seus processos e que acreditam na colaboração entre as equipes.",
+
+    expertiseTitle: "Especialidades",
+    highlightTitle: "Destaques",
+
+    expertise1: [
+      "Gestão de equipes de design",
+      "Gestão de stakeholders",
+      "Framework de excelência",
+      "Pesquisa exploratória",
+      "Sessões de testes de usabilidade",
+      "Pesquisa de mercado"
+    ],
+
+      expertise2: [
+      "Relatórios e auditorias de UX",
+      "Evangelização de UX",
+      "Mentoria de designers para entregas de qualidade",
+      "Design thinking",
+      "Prototipagem",
+      "Handoff colaborativo com devs"
+    ],
+
+    highlights1: [
+      "Certificado em UX (UXC) pelo Nielsen Norman Group e Second Wave Dive (Pensamento Estratégico de Negócios)."
+    ],
+
+    highlights2: [
+      "Experiência em agências, startups e empresas corporativas para entrega de websites e aplicativos nativos."
+    ],
+
+    highlights3: [
+      "Palestrante em diversos eventos sobre design, experiência do usuário, design systems e gestão de equipes."
+    ],
+
+    highlights4: [
+      "Fundador e instrutor na Ferrão Design, treinamento imersivo em UX com mais de 4500 alunos no Brasil."
+    ],
+
+
+    footerHeading: "Vamos conversar sobre design, marketing, negócios ou um dos meus hobbies como xadrez ou café, talvez?",
+    copyright: "Copyright © 2025 Igor Ferrão.",
+    rights: "Todos os direitos reservados."
   },
+
   en: {
     hi: "Hi,",
     name: "I'm Igor Ferrão",
@@ -17,19 +64,108 @@ const translations = {
     paragraph1: "Product Designer (UX/UI) with over 5 years of experience in creating scalable, functional, and user-centered digital products.",
     paragraph2: "I work from concept to delivery of complete digital solutions, focusing on the creation of Design Systems, advanced components in Figma, and efficient interfaces that ensure consistency across design and development teams.",
     paragraph3: "I hold a degree in Information Systems, which allows me to bridge design and technology effectively, collaborating with multidisciplinary teams in agile and remote environments.",
-    paragraph4: "I apply methods such as: Information Architecture | Low and High-Fidelity Prototyping | Usability Testing | Qualitative and Quantitative Research | User-Centered Design | Design Thinking | Agile | Lean UX",
+    paragraph4: "I apply methods such as:<br>Information Architecture | Low and High-Fidelity Prototyping | Usability Testing | Qualitative and Quantitative Research | User-Centered Design | Design Thinking | Agile | Lean UX",
     paragraph5: "I believe in design that balances aesthetics, accessibility, strategy, and results. Guided by strong values, I’m always looking to create experiences that truly make a positive impact.",
-    contact: "Contact"
+    contact: "Contact",
+
+    originTitle: "Igor Ferrão, born and based in Brazil.",
+    originText: "Working with companies that have UX as part of their process, belief in collaborative working between teams.",
+
+    expertiseTitle: "Expertise",
+    highlightTitle: "Highlights",
+
+    expertise1: [
+      "Managing design teams",
+      "Stakeholder management",
+      "Excellence framework",
+      "Discovery research",
+      "Usability testing sessions",
+      "Market research"
+    ],
+
+    expertise2: [
+      "UX reports and audits",
+      "Evangelizing UX",
+      "Coach designers to deliver quality",
+      "Design thinking",
+      "Prototyping",
+      "Collaborative handoff with devs"
+    ],
+
+    highlights1: [
+      "UXC (UX Certified) by the Nielsen Norman Group and Second Wave Dive (Strategic Business Thinking)."
+    ],
+
+    highlights2: [
+      "Background working with design agencies, startups and corporate companies to deliver websites and native apps."
+    ],
+
+    highlights3: [
+      "Spoke at numerous events about design, user experience, design systems and team management."
+    ],
+
+    highlights4: [
+      "Founder and Instructor @ Ferrão Design, an immersive User Experience training with 4500+ alumni in Brazil."
+    ],
+
+    footerHeading: "Let's talk about design, marketing, business, or one of my hobbies like chess or coffee, maybe?",
+    copyright: "Copyright © 2025 Igor Ferrão.",
+    rights: "All Rights Reserved."
   },
+
   es: {
     hi: "Hola,",
     name: "Soy Igor Ferrão",
-    role: "UX UI Designer en Xgrow Learning Platform",
+    role: "Diseñador UX UI en Xgrow Learning Platform",
     paragraph1: "Diseñador de Productos (UX/UI) con más de 5 años de experiencia en la creación de productos digitales escalables, funcionales y centrados en el usuario.",
-    paragraph2: "Trabajo desde el concepto hasta la entrega de soluciones digitales completas, con enfoque en la creación de Design Systems, componentes avanzados en Figma e interfaces eficientes que aseguran consistencia entre equipos de diseño y desarrollo.",
-    paragraph3: "Soy graduado en Sistemas de Información, lo que me permite unir diseño y tecnología de forma efectiva, colaborando con equipos multidisciplinarios en entornos ágiles y remotos.",
-    paragraph4: "Aplico métodos como: Arquitectura de Información | Prototipos de Baja y Alta Fidelidad | Pruebas de Usabilidad | Investigaciones Cualitativas y Cuantitativas | Diseño Centrado en el Usuario | Design Thinking | Ágil | Lean UX",
-    paragraph5: "Creo en un diseño que equilibre estética, accesibilidad, estrategia y resultados. Guiado por valores sólidos, siempre busco crear experiencias que realmente generen un impacto positivo.",
-    contact: "Contacto"
+    paragraph2: "Trabajo desde el concepto hasta la entrega de soluciones digitales completas, enfocado en la creación de Design Systems, componentes avanzados en Figma e interfaces eficientes que garantizan la coherencia entre diseño y desarrollo.",
+    paragraph3: "Soy licenciado en Sistemas de Información, lo que me permite conectar el diseño y la tecnología de manera eficaz, colaborando con equipos multidisciplinarios en entornos ágiles y remotos.",
+    paragraph4: "Aplico métodos como:<br>Arquitectura de la Información | Prototipado de baja y alta fidelidad | Pruebas de usabilidad | Investigación cualitativa y cuantitativa | Diseño centrado en el usuario | Design Thinking | Ágil | Lean UX",
+    paragraph5: "Creo en un diseño que equilibra estética, accesibilidad, estrategia y resultados. Guiado por valores sólidos, siempre busco crear experiencias que realmente generen un impacto positivo.",
+    contact: "Contacto",
+
+    originTitle: "Igor Ferrão, nacido y radicado en Brasil.",
+    originText: "Trabajando con empresas que tienen UX como parte de sus procesos, con creencia en el trabajo colaborativo entre equipos.",
+
+    expertiseTitle: "Especialidades",
+    highlightTitle: "Destacados",
+
+    expertise1: [
+      "Gestión de equipos de diseño",
+      "Gestión de stakeholders",
+      "Marco de excelencia",
+      "Investigación exploratoria",
+      "Sesiones de pruebas de usabilidad",
+      "Investigación de mercado"
+    ],
+
+    expertise2: [
+      "Informes y auditorías de UX",
+      "Evangelización de UX",
+      "Mentoría para diseñadores",
+      "Design thinking",
+      "Prototipado",
+      "Handoff colaborativo con desarrolladores"
+    ],
+
+    highlights1: [
+      "Certificado en UX (UXC) por Nielsen Norman Group y Second Wave Dive (Pensamiento Estratégico de Negocios)."
+    ],
+
+    highlights2: [
+      "Experiencia en agencias de diseño, startups y empresas corporativas para entregar sitios web y aplicaciones nativas."
+    ],
+
+    highlights3: [
+      "Conferencista en numerosos eventos sobre diseño, experiencia de usuario, design systems y gestión de equipos."
+    ],
+
+    highlights4: [
+      "Fundador e instructor en Ferrão Design, capacitación inmersiva en UX con más de 4500 alumnos en Brasil."
+    ],
+
+    footerHeading: "Hablemos sobre diseño, marketing, negocios o uno de mis pasatiempos como ajedrez o café, ¿tal vez?",
+    copyright: "Copyright © 2025 Igor Ferrão.",
+    rights: "Todos los derechos reservados."
   }
 };
